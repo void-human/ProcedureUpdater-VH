80faba7ef6a1dae6678c3301306d109d97f098c5
Luis Fernando Robles <75143017+void-human@users.noreply.github.com>
2021-02-12 17:36:29 -0600

    Modificacion de script para validaciones

--- ProcedureUpdater-VH/SQL/Script_Procedures.sql
+++ ProcedureUpdater-VH/SQL/Script_Procedures.sql
@@ -6,5 +6,12 @@ FROM
 	INNER JOIN sys.objects O ON M.object_id = O.object_id 
 WHERE 
 	type_desc = 'SQL_STORED_PROCEDURE' 
+	AND O.name NOT IN ('sp_alterdiagram'
+						, 'sp_creatediagram'
+						, 'sp_dropdiagram'
+						, 'sp_helpdiagramdefinition'
+						, 'sp_helpdiagrams'
+						, 'sp_renamediagram'
+						, 'sp_upgraddiagrams')
 ORDER BY 
 	O.name
